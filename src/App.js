f437198572b6843da071da9cad01a0ae64f32c85
Jkierem <juanmiguelgomez8@gmail.com>
2021-08-04 11:23:42 -0500

    Fixing help typo

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const Version = () => {
     rel="noreferrer" 
     target="_blank" 
     href="https://github.com/Jkierem/snaker">
-      v1.1.0 
+      v1.1.1
     </a>
     <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" title="This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.">
       <img alt="Creative Commons License" style={{"border-width":"0"}} src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" />
--- src/components/HelpModal/index.js
+++ src/components/HelpModal/index.js
@@ -45,7 +45,7 @@ const HelpModal = ({ open, onClose }) => {
             <pre>{worldType}</pre>
             <pre>{useStateType}</pre>
             <p>If moveForward, turnLeft and turnRight are all called on a single run, the last call wins. Not calling any function has the same effect as calling moveForward</p>
-            <p>The World is a two dimensional array where the top left corner is World[0][0]. The tile in row 1 and column 4 is World[0][3]. Each value is either a 0 for empty space, a 1 for fruit and 2 for an obstacle. The world does not include the snake's body</p>
+            <p>The World is a two dimensional array where the top left corner is World[0][0]. The tile in column 1 and row 4 is World[0][3]. Each value is either a 0 for empty space, a 1 for fruit and 2 for an obstacle. The world does not include the snake's body</p>
             <p>The useState function is used to preserve a value over a run. It recieves an ID and initial value and returns an array with the current value and a setter function. This is the only way to preserve values between ticks</p>
             <p>Auto saved is enabled. This game uses localStorage to save your code. Below the editor is a small debugger</p>
             <div>
